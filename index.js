8cd13a313f972eab070e04187b3c87f8d922abc1
Poppy Puspa Sari <poppy.puspasari@yahoo.co.id>
2019-07-08 14:47:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,4 +2,6 @@ var add = (a, b) => +a + +b
 var substract = (a, b) => +a - +b
 var multiply = (a, b) => +a * +b
 var divide = (a, b) => +a / +b
-var inc = (n) =>
+var inc = (n) => +n++ 
+var dec = (n) => +n-- 
+var makeInt = n => parseInt(n, 10)
